3a3043938cd629a5d1a8d7871bf55bfbae8b2aec
Влад Белоусов <vladbelousov@MacBook-Air-rekvid.local>
2025-11-06 23:15:37 +0300

    transfer layer + logic add

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,2 +1,36 @@
 package auth
 
+import (
+	"context"
+	"fmt"
+
+	authpb "auth2/api/proto"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type AuthHandler struct {
+	authpb.UnimplementedAuthServiceServer
+	svc *Service
+}
+
+func NewAuthHandler(svc *Service) *AuthHandler {
+	return &AuthHandler{svc: svc}
+}
+
+func (h *AuthHandler) Register(ctx context.Context, req *authpb.RegisterRequest) (*authpb.RegisterResponse, error) {
+	id, err := h.svc.Register(req.Email, req.Password)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	return &authpb.RegisterResponse{Token: fmt.Sprintf("user_%d", id)}, nil
+}
+
+func (h *AuthHandler) Login(ctx context.Context, req *authpb.LoginRequest) (*authpb.LoginResponse, error) {
+	token, err := h.svc.Login(req.Email, req.Password)
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, err.Error())
+	}
+	return &authpb.LoginResponse{Token: token}, nil
+}
--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,2 +1,48 @@
 package auth
 
+import (
+	"auth2/internal/store"
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Service struct {
+	store  *store.Store
+	jwtKey []byte
+}
+
+func NewService(store *store.Store, jwtKey string) *Service {
+	return &Service{store: store, jwtKey: []byte(jwtKey)}
+}
+
+func (s *Service) Register(email, password string) (uint, error) {
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	err := s.store.CreateUser(email, string(hash))
+	if err != nil {
+		return 0, err
+	}
+	user, _ := s.store.GetUserByEmail(email)
+	return user.ID, nil
+}
+
+func (s *Service) Login(email, password string) (string, error) {
+	user, err := s.store.GetUserByEmail(email)
+	if err != nil {
+		return "", fmt.Errorf("user not found")
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) != nil {
+		return "", fmt.Errorf("invalid password")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+	})
+	tokenStr, err := token.SignedString(s.jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenStr, nil
+}
